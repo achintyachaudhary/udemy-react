d995375321a8419e762efdd08899b14a4a2319eb
Achintya Ranjan Chaudhary <achintyaranjanchaudhary@Achintyas-MacBook-Air.local>
2023-07-02 19:52:01 +0530

    .

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css'
+function ExpenseItem() {
+    return (
+        <div className='expense-item'>
+            <div>March 28, 2023</div>
+            <div className='expense-item__description'>
+                <h2>car insurance</h2>
+                <div className='expense-item__price'>500</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
